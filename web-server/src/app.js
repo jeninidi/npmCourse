f3e3da47e88c018612895db78eb0dbbd3c0201a0
Zheni Nikolaeva Dimitrova <zhen0101@stud.kea.dk>
2019-03-22 12:36:14 +0100

    web server app with front end completed

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -10,7 +10,7 @@ const app = express()
 //define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname,'../templates/partials')
+const partialsPath = path.join(__dirname, '../templates/partials')
 hbs.registerPartials(partialsPath)
 
 //set up handlebars engine and views location
@@ -31,33 +31,53 @@ app.get('', (req, res) => {
     res.send('<h1>Weather</h1>')
 })
 
- app.get('/help', (req, res) => {
-     res.render('help', {
-         what: 'some documentation',
-         subject: 'node with express',
-         title: 'Help',
-         name: 'Zheni Dimitrova'
+app.get('/help', (req, res) => {
+    res.render('help', {
+        what: 'some documentation',
+        subject: 'node with express',
+        title: 'Help',
+        name: 'Zheni Dimitrova'
 
-     })
+    })
 })
 
- app.get('/about', (req, res) => {
+app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About me',
         name: 'Zheni Dimitrova'
     })
- })
+})
 
 app.get('/weather', (req, res) => {
     res.send([{
         forecast: 'rainy',
         location: 'Copenhagen'
-    },{
+    }, {
         forecast: 'sunny',
         location: 'New York'
     }])
 })
 
-app.listen(3000, ()=>{
-    console.log('Server is up on port 3000')
+app.get('/help/*', (req, res) => {
+    res.render( '404', {
+        title: '404',
+        name: 'Zheni Dimitova',
+        errorMessage: 'Help article hot found'
+    }
+
+    )
 })
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: ' 404',
+        name: 'Zheni Dimitrova',
+        errorMassage: 'Page not found'
+                
+    })
+})
+
+        app.listen(3000, () => {
+            console.log('Server is up on port 3000')
+
+        })
