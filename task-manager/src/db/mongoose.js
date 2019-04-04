5fc363f42ab517e722232baa99e63bc8c29f6e69
Zheni Nikolaeva Dimitrova <zhen0101@stud.kea.dk>
2019-04-04 09:28:25 +0200

    new features an bug fixing

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,62 +1,9 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
-
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minlenght: 7,
-        trim: true,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('Password cannot contain the keyword password')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number!')
-            }
-        }
-    }
-})
-
-// const me = new User({
-//     name: '   Anne   ',
-//     age: 32,
-//     email: 'anne@ANNE.anNe',
-//     password: 'dfjhfghdg2435'
-// })
-
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
 
 const Task = mongoose.model('Task', {
     description: {
--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,7 +1,23 @@
 const express = require('express')
+require('./db/mongoose.js')
+const User = require('./models/user.js')
+
+
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.post('/users', (req, res) => {
+    const user = new User(req.body)
+
+    user.save().then(() => {
+        res.send(user)
+    }).catch(() => {
+
+    })
+})
 
 app.listen(port, () => {
     console.log('Server is up and running on port ' + port)
