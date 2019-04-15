25ef30f80e379e1eacc53bfe67eb9cdd12833d2a
Zheni Nikolaeva Dimitrova <zhen0101@stud.kea.dk>
2019-04-15 13:18:17 +0200

    moving task and user routes to separate files

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -2,194 +2,22 @@ const express = require('express')
 const mongoose = require('./db/mongoose')
 const User = require('./models/user')
 const Task = require('./models/task')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(userRouter) 
+app.use(taskRouter)
 
-//users
-
-//update user
-app.patch('/users/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'password', 'age']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' })
-    }
-
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        })
-        if (!user) {
-            return res.status(404).send({ error: 'User not found!' })
-        }
-        res.send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
+//adding new router
+const router = new express.Router()
+router.get('/test', (req, res) => {
+    res.send('Thisis from my other router')
 })
-
-//create new user
-app.post('/users', async (req, res) => {
-    const user = new User(req.body)
-    try {
-        await user.save()
-        res.status(201).send(user)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-    // user.save().then(() => {
-    //     res.status(201).send(user)
-    // }).catch((e) => {
-    //     res.status(400).send(e)
-    // })
-})
-
-//display all users
-app.get('/users', async (req, res) => {
-    try {
-        const users = await User.find({})
-        res.send(users)
-    } catch (e) {
-        res.status(500).send()
-    }
-    // User.find({}).then((users) => {
-    //     res.send(users)
-    // }).catch((e) => {
-    //     res.status(500).send()
-    // })
-})
-
-//get user by id
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-    try {
-        const user = await User.findById(_id)
-        if (!user) {
-            return res.status(404).send()
-        }
-        res.send(user)
-    } catch (e) {
-        res.status(500).send
-    }
-    //  console.log(req.params)
-    // User.findById(_id).then((user) => {
-    //     if (!user) {
-    //         return res.status(404).send()
-    //     }
-    //     res.send(user)
-    // }).catch((e) => {
-    //     res.status(500).send()
-    // })
-})
-
-//delete user
-app.delete('/users/:id', async (req, res) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.id)
-        if (!user) {
-            return res.status(404).send({ error: 'User not found!' })
-        }
-        res.send(user)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
-
-//tasks
-
-//update task
-app.patch('/tasks/:id', async (req, res) => {
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['completed', 'description']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-    if (!isValidOperation) {
-        return res.status(400).send({ error: 'Invalid updates!' })
-    }
-
-    try {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true
-        })
-        if (!task) {
-            return res.status(404).send({ error: 'Task does not exist!' })
-        }
-        res.send(task)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-})
-
-//add new task
-app.post('/tasks', async (req, res) => {
-    const task = new Task(req.body)
-    try {
-        await task.save()
-        res.status(201).send(task)
-    } catch (e) {
-        res.status(400).send(e)
-    }
-    // task.save().then(() => {
-    //     res.status(201).send(task)
-    // }).catch((e) => {
-    //     res.status(400).send(e)
-    // })
-})
-
-//display all tasks
-app.get('/tasks', async (req, res) => {
-    try {
-        const tasks = await Task.find({})
-        res.send(tasks)
-    } catch (e) {
-        res.status(500).send()
-    }
-    // Task.find({}).then((tasks) => {
-    //     res.send(tasks)
-    // }).catch((e) => {
-    //     res.status(500).send()
-    // })
-})
-
-//get task by id
-app.get('/tasks/:id', async (req, res) => {
-    const _id = req.params.id
-    // Task.findById(_id).then((task) => {
-    //     if (!task) {
-    //         return res.status(404).send()
-    //     }
-    //     res.send(task)
-    // }).catch((e) => {
-    //     res.status(500).send()
-    // })
-    try {
-        const task = await Task.findById(_id)
-        if (!task) {
-            return res.status(404).send()
-        }
-        res.send(task)
-    } catch (e) {
-        res.status(500).send
-    }
-})
-
-//delete task
-app.delete('/tasks/:id', async (req, res) => {
-    try {
-        const task = await Task.findByIdAndDelete(req.params.id)
-        if (!task) {
-            return res.status(404).send({ error: 'Task not found!' })
-        }
-        res.send(task)
-    } catch (e) {
-        res.status(500).send()
-    }
-})
+app.use(router)
 
 //listening on port
 app.listen(port, () => {
---
+++ task-manager/src/routers/task.js
@@ -0,0 +1,93 @@
+const express = require('express')
+const Task = require('../models/task')
+const router = new express.Router()
+
+//update 
+router.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['completed', 'description']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        })
+        if (!task) {
+            return res.status(404).send({ error: 'Task does not exist!' })
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+//add new 
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body)
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+    // task.save().then(() => {
+    //     res.status(201).send(task)
+    // }).catch((e) => {
+    //     res.status(400).send(e)
+    // })
+})
+
+//read all 
+router.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch (e) {
+        res.status(500).send()
+    }
+    // Task.find({}).then((tasks) => {
+    //     res.send(tasks)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
+})
+
+//get task by id
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+    // Task.findById(_id).then((task) => {
+    //     if (!task) {
+    //         return res.status(404).send()
+    //     }
+    //     res.send(task)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
+    try {
+        const task = await Task.findById(_id)
+        if (!task) {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send
+    }
+})
+
+//delete task
+router.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if (!task) {
+            return res.status(404).send({ error: 'Task not found!' })
+        }
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+module.exports = router
